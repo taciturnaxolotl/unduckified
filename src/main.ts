8e3827343eabf6f7e4f287e482ffe18704187fd0
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2025-03-03 03:53:23 -0500

    chore: use local font thnks to @ronanru

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { bangs } from "./hashbang.ts";
+
+import "@fontsource/inter/latin-400.css";
+import "@fontsource/inter/latin-500.css";
+import "@fontsource/inter/latin-600.css";
+import "@fontsource/inter/latin-700.css";
 import "./global.css";
 
 const CONSTANTS = {
