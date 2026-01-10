74f772eed1f23cb3bdf56205784cf38fca06da65
Kieran Klukas <me@dunkirk.sh>
2026-01-09 22:25:14 -0800

    bug: add kagi bangs properly

--- src/bangs/hashbanggen.ts
+++ src/bangs/hashbanggen.ts
@@ -31,6 +31,86 @@ const hashbang: Record<string, RuntimeBang> = {
 		s: "Duckduckgo images",
 		u: "https://duckduckgo.com/?q={{{s}}}&ia=images&iax=images&atb=v375-1",
 	},
+	k: {
+		d: "kagi.com",
+		s: "Kagi Search",
+		u: "https://kagi.com/search?q={{{s}}}",
+	},
+	kagi: {
+		d: "kagi.com",
+		s: "Kagi Search",
+		u: "https://kagi.com/search?q={{{s}}}",
+	},
+	ki: {
+		d: "kagi.com",
+		s: "Kagi Images",
+		u: "https://kagi.com/images?q={{{s}}}",
+	},
+	kagii: {
+		d: "kagi.com",
+		s: "Kagi Images",
+		u: "https://kagi.com/images?q={{{s}}}",
+	},
+	kv: {
+		d: "kagi.com",
+		s: "Kagi Videos",
+		u: "https://kagi.com/videos?q={{{s}}}",
+	},
+	kagiv: {
+		d: "kagi.com",
+		s: "Kagi Videos",
+		u: "https://kagi.com/videos?q={{{s}}}",
+	},
+	kn: {
+		d: "kagi.com",
+		s: "Kagi News",
+		u: "https://kagi.com/news?q={{{s}}}",
+	},
+	kagin: {
+		d: "kagi.com",
+		s: "Kagi News",
+		u: "https://kagi.com/news?q={{{s}}}",
+	},
+	km: {
+		d: "kagi.com",
+		s: "Kagi Maps",
+		u: "https://kagi.com/maps?q={{{s}}}",
+	},
+	kagim: {
+		d: "kagi.com",
+		s: "Kagi Maps",
+		u: "https://kagi.com/maps?q={{{s}}}",
+	},
+	kp: {
+		d: "kagi.com",
+		s: "Kagi Podcasts",
+		u: "https://kagi.com/podcasts?q={{{s}}}",
+	},
+	kagip: {
+		d: "kagi.com",
+		s: "Kagi Podcasts",
+		u: "https://kagi.com/podcasts?q={{{s}}}",
+	},
+	kf: {
+		d: "kagi.com",
+		s: "Kagi FastGPT",
+		u: "https://kagi.com/fastgpt?q={{{s}}}",
+	},
+	fastgpt: {
+		d: "kagi.com",
+		s: "Kagi FastGPT",
+		u: "https://kagi.com/fastgpt?q={{{s}}}",
+	},
+	ka: {
+		d: "kagi.com",
+		s: "Kagi Assistant",
+		u: "https://kagi.com/assistant?q={{{s}}}",
+	},
+	assistant: {
+		d: "kagi.com",
+		s: "Kagi Assistant",
+		u: "https://kagi.com/assistant?q={{{s}}}",
+	},
 };
 
 rawBangs.forEach((bang: any) => {
